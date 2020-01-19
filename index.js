1b0b501ad16ba9f66407215aa28baf3957dbc364
Francisco Giordano <frangio.1@gmail.com>
2020-01-18 22:05:13 -0300

    inline everything in one file

--- index.js
+++ index.js
@@ -3,9 +3,48 @@
 const proc = require('child_process');
 const events = require('events');
 
-const { run, Pool } = require('./pool');
+/**
+ * @typedef {'ignore' | 'inherit'} Stdio
+ * @param {string} cmd
+ * @param {Stdio} stdio
+ */
+function run(cmd, stdio = 'ignore') {
+  return proc.spawn(cmd, { stdio, shell: true });
+}
 
-main();
+class Pool {
+  /**
+   * @param {string[]} cmds
+   */
+  constructor(cmds) {
+    this.children = new Map(
+      cmds.map(cmd => [run(cmd), cmd])
+    );
+  }
+
+  killAll() {
+    for (const c of this.children.keys()) {
+      c.kill();
+    }
+  }
+
+  get someExit() {
+    const childrenExit = Array.from(
+      this.children.keys(),
+      async cmd => {
+        const [code] =
+          /** @type {[number]} */
+          (await events.once(cmd, 'exit'));
+
+        this.exit = {
+          code,
+          cmd: this.children.get(cmd),
+        };
+      }
+    );
+    return Promise.race(childrenExit);
+  }
+}
 
 async function main() {
   if (process.argv.length < 3) {
@@ -34,3 +73,5 @@ async function main() {
     process.exitCode = 1;
   }
 }
+
+main();
